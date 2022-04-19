bc276e3a9e0cf8ea2cb7d0f54841e5454a0ba405
tobythatsall <93424907+tobythatsall@users.noreply.github.com>
2022-04-19 11:14:40 +1000

    setting up the skeleton
    adding in comments, the main variable, stuff like that

--- main.ts
+++ main.ts
@@ -1,10 +1,16 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 // Remember to rename these classes and interfaces!
+let userTag = '#quotes';
 
 export default class MyPlugin extends Plugin {
 	onload() {
-		// thinking
+		// search the vault for the userTag
+
+
+		// display results
+
+		
 	}
 
 	onunload() {
