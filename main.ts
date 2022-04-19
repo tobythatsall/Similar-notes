5d6a4838899cde87ce3771fdf8d31f29aebada1b
tobythatsall <93424907+tobythatsall@users.noreply.github.com>
2022-04-20 08:30:05 +1000

    Setting up Dataview API

--- main.ts
+++ main.ts
@@ -1,4 +1,17 @@
 import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { getAPI, Values } from "obsidian-dataview"
+
+/* dataview stuff
+const field = getAPI(plugin.app)?.page('sample.md').field;
+if (!field) return;
+
+if (Values.isHtml(field)) // do something
+else if (Values.isLink(field)) // do something
+// ...
+
+getAPI(plugin.app)
+
+*/
 
 // Remember to rename these classes and interfaces!
 let userTag = '#quotes';
